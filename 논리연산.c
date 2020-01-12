700d6d6b6c892de8c85d872b6cdb9c3519810075
ckdgus16 <50093317+ckdgus16@users.noreply.github.com>
2020-01-12 16:29:11 +0900

    Create 논리연산.c

---
+++ 논리연산.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main() {
+	// 논리 연산자
+	/*int a;
+	printf("아무 숫자나 입력하세요 : ");
+	scanf_s("%d", &a);
+
+	if (a >= 10 && a < 20)
+	{
+		printf(" %d 는 10 이상, 20 미만인 수 입니다. \n", a);
+	} */
+
+	// 논리합
+	/*int height, weight;
+	printf("당신의 키와 몸무게를 각각 입력해 주세요 :");
+	scanf_s("%d %d", &height, &weight);
+
+	if (height >= 190 || weight >= 100)
+	{
+		printf("당신은 '거구' 입니다. \n");
+	}
+	else
+	{
+		printf("당신은 '거구' 가 아닙니다. \n");
+	}
+
+	return 0;
+	*/
+}
