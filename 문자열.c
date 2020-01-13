e06a63571708c5a281bc1d682361b3b0121b2b6b
ckdgus16 <50093317+ckdgus16@users.noreply.github.com>
2020-01-13 13:28:08 +0900

    Update 문자열.c

--- 문자열.c
+++ 문자열.c
@@ -91,8 +91,8 @@
 //버퍼에 관한 이해
 //이상한 scanf
 
-int main() {
-	//int num;
+//int main() {
+//	//int num;
 	//char c;
 
 	//printf("숫자를 입력하세요 : ");
@@ -126,17 +126,17 @@ int main() {
 
 	//버퍼 비우기
 
-	int num; // int 정수 num 선언
-	char c; 
+	//int num; // int 정수 num 선언
+	//char c; 
 
-	getch(); //
-	printf("숫자를 입력하세요 : ");
-	scanf_s("%d", &num);
+	//getch(); //
+	//printf("숫자를 입력하세요 : ");
+	//scanf_s("%d", &num);
 
-	fflush(stdin); //작동을 안하는 듯!
+	//fflush(stdin); //작동을 안하는 듯!
 
-	printf("문자를 입력하세요 : ");
-	scanf_s("%c", &c, 10);
+	//printf("문자를 입력하세요 : ");
+	//scanf_s("%c", &c, 10);
 
 	//getchar 함수 이용
 
@@ -171,8 +171,112 @@ int main() {
 	str[1] = 'a';
 	pstr[1] = 'a';*/ //에러 구문
 
-
+	
 	
 
-	return 0;
-}
+//	return 0;
+//}
+
+//문자열 복사
+
+//char copy_str(char* src, char* dest);
+//int main() {
+//	char str1[] = "hello";
+//	char str2[] = "hi";
+//
+//	printf("복사 이전 : %s \n", str1);
+//
+//	copy_str(str1, str2);
+//
+//	printf("복사 이후 : %s \n", str1);
+//
+//	return 0;
+//}
+//char copy_str(char* dest, char* src) {  // 만약에 dest의 크기가 src의 크기보다 작다면 허락 되지 않는 공간까지 침범하므로 문제 발생, 검사가 필요하다. 위험한 함수이다....
+//	while (*src) {
+//		*dest = *src;
+//		src++;
+//		dest++;
+//	}
+//	*dest = '\0';
+//
+//	return 1;
+//}
+
+// 문자열을 합치는 함수
+
+//char stradd(char* dest, char* src);
+//int main() {
+//	char str1[100] = "hello my name is ";
+//	char str2[] = "Psi";
+//
+//	printf("합치기 이전 : %s \n", str1);
+//
+//	stradd(str1, str2);
+//
+//	printf("합친 이후 : %s \n", str1);
+//
+//	return 0;
+//}
+//char stradd(char* dest, char* src) {
+//	while (*dest) { // dest의 끝 부분을 찾는 것
+//		dest++;
+//	}// 구문이 끝나고 나서 dest는 dest의 끝부분 NULL 문자를 가리키고 있다.
+//	
+//	while (*src) { // src의 값의 끝까지 while문 구동 src의 문자열들을 dest 의 NULL 문자 있는 곳 부터 복사해 넣는다.
+//		*dest = *src; // src의 내용을 dest에 넣는다.
+//		src++; // 넣은 문자 다음 src의 값으로 증가
+//		dest++; // dest의 끝부분에서 src의 값이 들어가면 dest의 값 증가
+//	}
+//	*dest = '\0'; //마지막에 src의 NULL문자가 들어가지 못해서 dest에 NULL문자 추가
+//	return 1;
+//}
+
+//문자열을 비교하는 함수
+
+//char compare(char* str1, char* str2);
+//int main() {
+//	char str[20] = "hello every1";
+//	char str2[20] = "hello everyone";
+//	char str3[20] = "hello every1 hi";
+//	char str4[20] = "hello every1";
+//	
+//	if (compare(str, str2)) {
+//		printf("%s 와 %s 는 같다 \n", str, str2);
+//	}
+//	else {
+//		printf("%s 와 %s 는 다르다 \n", str, str2);
+//	}
+//
+//	if (compare(str, str3)) {
+//		printf("%s 와 %s 는 같다 \n", str, str3);
+//	}
+//	else {
+//		printf("%s 와 %s 는 다르다 \n", str, str3);
+//	}
+//
+//	if (compare(str, str4)) {
+//		printf("%s 와 %s 는 같다 \n", str, str4);
+//	}
+//	else {
+//		printf("%s 와 %s 는 다르다 \n", str, str4);
+//	}	   	 
+//}
+//
+//char compare(char* str1, char* str2) {
+//	while (*str1) {
+//		if (*str1 != *str2) {
+//			return 0;
+//		}
+//
+//		str1++;
+//		str2++;
+//	}
+//	if (*str2 == '\0') {
+//		return 1;
+//	}
+//	return 0;
+//}
+
+
+
