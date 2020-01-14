0f40994d56742cdef85f306c21eb68bc75b18783
ckdgus16 <50093317+ckdgus16@users.noreply.github.com>
2020-01-14 16:11:28 +0900

    Update struct.c

--- struct.c
+++ struct.c
@@ -305,3 +305,132 @@
 //
 //	return A;
 //}
+
+//구조체 변수의 정의 방법
+
+//char copy_str(char* dest, char* src);
+//int Print_Obj_Status(struct obj OBJ);
+//struct obj {
+//	char name[20];
+//	int x, y;
+//}Ball;
+//
+//int main() {
+//	Ball.x = 3;
+//	Ball.y = 4;
+//	copy_str(Ball.name, "RED BALL");
+//
+//	Print_Obj_Status(Ball);
+//	return 0;
+//}
+//
+//int Print_Obj_Status(struct obj OBJ) {
+//	printf("Location of %s \n", OBJ.name);
+//	printf("( %d , %d ) \n", OBJ.x, OBJ.y);
+//
+//	return 0;
+//}
+//char copy_str(char* dest, char* src) {
+//	while (*src) {
+//		*dest = *src;
+//		src++;
+//		dest++;
+//	}
+//	*dest = '\0';
+//	return 1;
+//}
+
+//멤버를 쉽게 초기화 하기
+
+//int Print_Status(struct HUMAN human);
+//struct HUMAN {
+//	int age;
+//	int height;
+//	int weight;
+//	int gender;
+//};
+//
+//int main() {
+//	struct HUMAN Adam = { 31, 182, 75, 0 };
+//	struct HUMAN Eve = { 27, 166, 48, 1 };
+//
+//	Print_Status(Adam);
+//	Print_Status(Eve);
+//}
+//
+//int Print_Status(struct HUMAN human) {
+//	if (human.gender == 0) {
+//		printf("MALE \n");
+//	}
+//	else {
+//		printf("FEMALE \n");
+//	}
+//	printf("AGE : %d / Height : %d / Weight : %d \n ", human.age, human.height, human.weight);
+//
+//	if (human.gender == 0 && human.height >= 180) {
+//		printf("HE IS A WINNER!! \n");
+//	}
+//	else if (human.gender == 0 && human.height < 180) {
+//		printf("HE IS A LOSER!! \n");
+//	}
+//	printf("----------------------------------------- \n");
+//	return 0;
+//}
+
+
+//UNION 공용체
+//구조체와 달리 메모리를 '공유'한다.
+
+//union A {
+//	int i;
+//	char j;
+//};
+//int main() {
+//	union A a;
+//	a.i = 0x12345678;
+//	printf("%x", a.j);
+//	return 0;
+//}
+
+//빅 엔디안(Big Endian), 리틀 엔디안(little Endian)
+
+//union A {
+//	int i;
+//	short j;
+//};
+//int main() {
+//	union A a;
+//	a.i = 0x12345678;
+//	printf("%x", a.j);
+//	return 0;
+//}
+
+
+//열거형
+
+//enum{RED,BLUE,WHITE,BLACK};
+//int main() {
+//	int palette = 1;
+//	//scanf_s("%d", &palette);
+//	switch (palette) {
+//	case RED:
+//		printf("palette : RED \n");
+//		break;
+//	case BLUE:
+//		printf("palette : BLUE \n");
+//		break;
+//	case WHITE:
+//		printf("palette : WHITE \n");
+//		break;
+//	case BLACK:
+//		printf("palette : BLACK \n");
+//		break;
+//	}
+//}
+
+//enum{RED = 3, BLUE, WHITE, BLACK};
+//int main() {
+//	int palette = BLACK;
+//	printf("%d\n", palette);
+//}
+
