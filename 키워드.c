be615b821a8820a42b1b5248b14cb2281746f4f2
ckdgus16 <50093317+ckdgus16@users.noreply.github.com>
2020-01-16 14:17:25 +0900

    Create 키워드.c

---
+++ 키워드.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include"Weird.h"
+#include"Weird.h"
+
+//typedef int cal;
+//
+//int main() {
+//	cal input;
+//	cal a, b;
+//	while (1) {
+//		printf("--- 계산기 -- \n");
+//		printf("1. 덧셈 \n");
+//		printf("2. 뺄셈 \n");
+//		printf("3. 종료 \n");
+//
+//		scanf_s("%d", &input);
+//
+//		if (input == 1) {
+//			printf("두 수 : ");
+//			scanf_s("%d%d", &a, &b);
+//			printf("%d와 %d 의 합 : %d \n", a, b, a + b);
+//		}
+//		else if (input == 2) {
+//			printf("두수 : ");
+//			scanf_s("%d%d", &a, &b);
+//			printf("%d와 %d의 차 : %d \n", a, b, a - b);
+//		}
+//		else
+//			break;
+//	}
+//	return 0;
+//}
+
+
+//int add(int a, int b);
+//int add(int a, int b) {
+//	return a + b;
+//}
+//typedef int CAL;
+//typedef int (*Padd)(int, int);
+//typedef int Arrays[10];
+//int main() {
+//	CAL a = 10;
+//	Arrays arr = { 1,2,3,4,5,6,7,8,9 };
+//	Padd ptr = add;
+//
+//	printf("a : %d \n", a);
+//	printf("arr[3] : %d \n", arr[3]);
+//	printf("add(3, 5 : %d \n", ptr(3, 5));
+//	return 0;
+//}
+
+
+//pragma 키워드
+
+//pragma pack
+//#pragma pack(1)
+//struct Weird {
+//	char arr[2];
+//	int i;
+//};
+//
+//int main() {
+//	struct Weird a;
+//	printf("size of a : %d \n", sizeof(a));
+//	return 0;
+//}
+
+//pragma once
+//int main() {
+//	struct Weird a;
+//	a.i = 3;
+//	printf("Weird 구조체의 a.i : %d \n", a.i);
+//
+//	return 0;
+//}
+
+
